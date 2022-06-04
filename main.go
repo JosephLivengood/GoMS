2670d985af5a7cfd1a11c7295d79443fe3fe7983
Joseph Livengood <josephlivengood@Josephs-MacBook-Pro.local>
2022-06-04 11:22:23 -0400

    Initial commit

---
+++ main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"net/http"
+	"time"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
+)
+
+func main() {
+	r := chi.NewRouter()
+	r.Use(middleware.RequestID)
+	r.Use(middleware.RealIP)
+	r.Use(middleware.Logger)
+	r.Use(middleware.Recoverer)
+
+	r.Use(middleware.Timeout(60 * time.Second))
+
+	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
+		_, _ = w.Write([]byte("welcome"))
+	})
+
+	err := http.ListenAndServe(":3000", r)
+	if err != nil {
+		return
+	}
+}
